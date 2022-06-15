f52c64b995cb386d07fcd50313686385e8fe9309
Vishal Verma <black2020panther@gmail.com>
2022-06-15 15:30:10 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+// import ShowData from './components/showdata';
+import Pagination from './components/pagination';
 
 function App() {
+
+  const [data, setData] = useState([
+    "item-1","item-2","item-3","item-4","item-5","item-6","item-7","item-8","item-9","item-10",
+    "item-11","item-12","item-13","item-14","item-15","item-16","item-17","item-18","item-19","item-20"
+  ]); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length > 0 ? (
+        <>
+        <Pagination
+        data={data}
+        title={"items"}
+        pageLimit={4}
+        dataLimit={5}
+        />
+        </>
+      ) : ( <h1>No Data to display</h1>)
+
+      } 
     </div>
   );
 }
---
+++ src/components/showdata.js
@@ -0,0 +1,9 @@
+
+const  ShowData = (props)=>{
+    return(
+        <div>
+            <h2>{props.data}</h2>
+        </div>
+    );
+}
+export default ShowData;
